62fa6b98c98eb54a1d81550b9cb61571a438178a
petepall <peter@ppallen.be>
2020-06-21 23:10:29 +0200

    Update user model, extend pagination

--- server/src/models/users.model.js
+++ server/src/models/users.model.js
@@ -3,17 +3,18 @@
 // See http://mongoosejs.com/docs/models.html
 // for more of what you can do here.
 module.exports = function (app) {
-  const modelName = 'users';
-  const mongooseClient = app.get('mongooseClient');
-  const schema = new mongooseClient.Schema({
-  
-    email: { type: String, unique: true, lowercase: true },
-    password: { type: String },
-  
-  
-  }, {
-    timestamps: true
-  });
+  const modelName = "users";
+  const mongooseClient = app.get("mongooseClient");
+  const schema = new mongooseClient.Schema(
+    {
+      email: { type: String, unique: true, lowercase: true },
+      password: { type: String, required: true },
+      displayName: { type: String, required: true },
+    },
+    {
+      timestamps: true,
+    }
+  );
 
   // This is necessary to avoid model compilation errors in watch mode
   // see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
@@ -21,5 +22,4 @@ module.exports = function (app) {
     mongooseClient.deleteModel(modelName);
   }
   return mongooseClient.model(modelName, schema);
-
 };
