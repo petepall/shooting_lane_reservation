8a5a7abf328adb272dcc8936d4f99a08320b3e87
petepall <peter@ppallen.be>
2020-06-21 20:33:19 +0200

    Implement dynamic menu options

--- src/components/navigation/TheNavigationDrawer.vue
+++ src/components/navigation/TheNavigationDrawer.vue
@@ -13,18 +13,24 @@
         rounded
         active-class="grey--text text--darken-4"
       >
-        <v-list-item :to="'/'">
+        <v-list-item
+          v-for="item in menuItems"
+          :key="item.title"
+          :to="item.link"
+        >
           <v-list-item-icon>
-            <v-icon>mdi-home</v-icon>
+            <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
-          <v-list-item-title>Home</v-list-item-title>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
         </v-list-item>
-
-        <v-list-item :to="'/about'">
+        <v-list-item
+          v-if="userIsAuthenticated"
+          @click="onLogout"
+        >
           <v-list-item-icon>
-            <v-icon>mdi-account</v-icon>
+            <v-icon>mdi-logout</v-icon>
           </v-list-item-icon>
-          <v-list-item-title>About</v-list-item-title>
+          <v-list-item-title>Logout</v-list-item-title>
         </v-list-item>
       </v-list-item-group>
     </v-list>
@@ -33,7 +39,13 @@
 
 <script>
 export default {
-  props: { value: { type: Boolean } },
+  props: {
+    value: { type: Boolean },
+    menuItems: {
+      type: Array[String],
+      default: []
+    }
+  },
   data() {
     return {
       drawer: false,
--- src/components/navigation/TheTopNavbar.vue
+++ src/components/navigation/TheTopNavbar.vue
@@ -4,10 +4,13 @@
       app
       color="grey lighten-5"
     >
-      <v-app-bar-nav-icon @click.prevent="$refs.drawer.drawer = !$refs.drawer.drawer" />
+      <v-app-bar-nav-icon
+        class="d-none d-flex d-sm-none"
+        @click.prevent="$refs.drawer.drawer = !$refs.drawer.drawer"
+      />
       <v-img
         src="../../assets/shooting-lane.png"
-        class="d-none d-md-block shooting-lane mx-3"
+        class=".d-none d-md-block shooting-lane mx-3"
       />
       <v-toolbar-title class="text-uppercase grey--text text--darken-3">
         {{ title }}
@@ -15,26 +18,33 @@
 
       <v-spacer />
 
-      <v-btn
-        class="d-none d-md-block grey--text text--darken-3"
-        text
-      >
-        <v-icon left>
-          mdi-login-variant
-        </v-icon>
-        Login
-      </v-btn>
-      <v-btn
-        class="d-none d-md-block grey--text text--darken-3"
-        text
-      >
-        <v-icon left>
-          mdi-logout-variant
-        </v-icon>
-        Logout
-      </v-btn>
+      <v-toolbar-items class="d-none d-sm-flex">
+        <v-btn
+          v-for="item in menuItems"
+          :key="item.title"
+          text
+          :to="item.link"
+        >
+          <v-icon left>
+            {{ item.icon }}
+          </v-icon>{{ item.title }}
+        </v-btn>
+        <v-btn
+          v-if="userIsAuthenticated"
+          text
+          @click="onLogout"
+        >
+          <v-icon left>
+            mdi-logout
+          </v-icon>
+          Logout
+        </v-btn>
+      </v-toolbar-items>
     </v-app-bar>
-    <TheNavigationDrawer ref="drawer" />
+    <TheNavigationDrawer
+      ref="drawer"
+      :menu-items="menuItems"
+    />
   </v-container>
 </template>
 
@@ -50,10 +60,37 @@ export default {
     },
   },
   data: () => ({
-
+    userIsAuthenticated: false
   }),
 
+  computed: {
+    menuItems() {
+      let menuItems = [
+        { icon: 'mdi-home', title: 'Home', link: '/' },
+        { icon: 'mdi-account', title: 'Sign-up', link: '/signUp' },
+        { icon: 'mdi-lock-open', title: 'Sign-in', link: '/signIn' }
+      ];
+      if (this.userIsAuthenticated) {
+        menuItems = [
+          { icon: 'mdi-home', title: 'Home', link: '/' },
+          {
+            icon: 'mdi-view-list',
+            title: 'Check availability',
+            link: '/availability'
+          },
+          {
+            icon: 'mdi-pin',
+            title: 'Reserve lane',
+            link: '/reserve'
+          },
+          { icon: 'mdi-face', title: 'Profile', link: '/profile' }
+        ];
+      }
+      return menuItems;
+    }
+  }
 };
+
 </script>
 
 <style scoped>
