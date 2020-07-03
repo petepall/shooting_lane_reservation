e280ab3347e5d5c7c784e62afbbe6c1e54052438
petepall <peter@ppallen.be>
2020-07-04 00:14:51 +0200

    Added route guard functionality

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -6,8 +6,26 @@ import Error from '../views/Error.vue';
 import SignUp from '../views/authentication/SignUp.vue';
 import SignIn from '../views/authentication/SingIn.vue';
 import Profile from '../views/user/Profile.vue';
+import Availability from '../views/reservations/Availability.vue';
+import LaneReservation from '../views/reservations/LaneReservation.vue';
+import store from '../store';
 
 Vue.use(VueRouter);
+
+function authGuard(to, from, next) {
+  store
+    .dispatch('auth/authenticate')
+    .then((response) => {
+      console.log(response.user);
+      next();
+    })
+    .catch((error) => {
+      if (!error.message.includes('Could not find stored JWT')) {
+        console.log('Authentication error', error);
+      }
+      next('/404');
+    });
+}
 
 const routes = [
   {
@@ -29,6 +47,22 @@ const routes = [
     path: '/profile',
     name: 'Profile',
     component: Profile,
+    meta: { requiresAuth: true },
+    beforeEnter: authGuard,
+  },
+  {
+    path: '/availability',
+    name: 'Availability',
+    component: Availability,
+    meta: { requiresAuth: true },
+    beforeEnter: authGuard,
+  },
+  {
+    path: '/reserve',
+    name: 'LaneReservation',
+    component: LaneReservation,
+    meta: { requiresAuth: true },
+    beforeEnter: authGuard,
   },
   {
     path: '/404',
