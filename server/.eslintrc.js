482c4e3dea21e265ddb9982415a0c52930a0753d
petepall <peter@ppallen.be>
2020-07-19 23:38:50 +0200

    Setup debug and update eslint rules

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -4,41 +4,15 @@ module.exports = {
     es6: true,
     browser: true,
   },
-  extends: ["plugin:vue/recommended", "@vue/airbnb"],
+  extends: ['prettier'],
+  plugins: ['prettier'],
   parserOptions: {
-    parser: "babel-eslint",
+    parser: 'babel-eslint',
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "comma-dangle": 0,
-    quotes: [2, "single", { avoidEscape: true }],
-    "vue/html-closing-bracket-newline": [
-      "error",
-      {
-        singleline: "never",
-        multiline: "always",
-      },
-    ],
-    "vue/max-attributes-per-line": [
-      2,
-      {
-        multiline: {
-          allowFirstLine: false,
-        },
-      },
-    ],
-    "vue/html-self-closing": [
-      "error",
-      {
-        html: {
-          void: "never",
-          normal: "always",
-          component: "always",
-        },
-        svg: "always",
-        math: "always",
-      },
-    ]
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'comma-dangle': 0,
+    quotes: [2, 'single', { avoidEscape: true }],
   },
 };
